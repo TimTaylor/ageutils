5f17b9a4e79c8636d0255455fa22e998f9caa1bb
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2025-10-13 12:29:59 +0100

    build

--- pkg/R/wip.R
+++
@@ -1,7 +0,0 @@
-ages_groups_to_factor <- function(x) {
-    lvls <- x[!is.na(x)]
-    lvls <- unique(lvls)
-    index <- as.integer(sub("\\[([[:digit:]]+),.*", "\\1", lvls))
-    index <- order(index)
-    factor(x, levels = lvls[index], ordered = TRUE)
-}
